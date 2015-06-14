ba451e3d669fd4ff2cc5367a48a1dd004d746207
Saleem Siddiqui <saleem@saleem-siddiqui.com>
2015-06-14 05:15:32 +0200

    @saleem refactored tests

--- lib/integer_sifter/baccab_sifter.rb
+++ lib/integer_sifter/baccab_sifter.rb
@@ -1,9 +1,9 @@
 class BACCABSifter
   def sift(n)
     case n % 6
-      when 1, 4  
+      when 1, 4
         return "A"
-      when 0, 5 
+      when 0, 5
         return "B"
       else return "C"
     end
--- test/test_sifting.rb
+++ test/test_sifting.rb
@@ -4,19 +4,19 @@ require 'integer_sifter'
 class HolaTest < Test::Unit::TestCase
   def test_ABBA_sifter
     sifter = IntegerSifter.create(:ABBA)
-    expected_A = [0, 3, 4, 7, 8, 11, 12]
-    expected_B = [1, 2, 5, 6, 9, 10, 13]
-    expected_A.each { |a| assert_equal "A", sifter.sift(a) }
-    expected_B.each { |b| assert_equal "B", sifter.sift(b) }
+    assert_pile [0, 3, 4, 7, 8, 11, 12], "A", sifter
+    assert_pile [1, 2, 5, 6, 9, 10, 13], "B", sifter
   end
 
   def test_BACCAB_sifter
     sifter = IntegerSifter.create(:BACCAB)
-    expected_A = [1, 4, 7, 10]
-    expected_B = [0, 5, 6, 11]
-    expected_C = [2, 3, 8, 9]
-    expected_A.each { |a| assert_equal "A", sifter.sift(a) }
-    expected_B.each { |b| assert_equal "B", sifter.sift(b) }
-    expected_C.each { |c| assert_equal "C", sifter.sift(c) }
+    assert_pile [1, 4, 7, 10], "A", sifter
+    assert_pile [0, 5, 6, 11], "B", sifter
+    assert_pile [2, 3, 8, 9], "C", sifter
+  end
+
+  private
+  def assert_pile(array, pile, sifter)
+    array.each { |e| assert_equal pile, sifter.sift(e) }
   end
 end
